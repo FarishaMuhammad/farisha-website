c454531524202063f684b52558f481d659f7c556
FarishaMuhammad <107351127+FarishaMuhammad@users.noreply.github.com>
2023-11-19 00:01:04 +0800

    Add files via upload

---
+++ headerAndFooter.js
@@ -0,0 +1,76 @@
+// Function to load the header
+function loadHeader() {
+	var header = document.createElement('header');
+	header.innerHTML = `
+    <link rel="stylesheet" href="styles.css" />
+        <a href="HP.html"> <img src="images/logo.png" alt="Logo">
+        </a>
+        <div class="main-nav">
+        
+            <nav>
+                <ul>
+                    <li><a href="biography.html" class="nav-link"> Biography </a></li>
+                    <li><a href="timetable.html" class="nav-link"> Timetable </a></li>
+                    <li><a href="galleries.html" class="nav-link"> Galleries </a></li>
+                    <li><a href="downloads.html" class="nav-link"> Download </a></li>
+                    <li>
+                        <div class="dropdown" id="linksDropdown"> 
+                            <a href="#"> Links </a>
+                            <div class="dropdown-content">
+                                <a href="https://uitm.edu.my/">UiTM Website</a>
+                                <a href="https://kppim.uitm.edu.my/">KPPIM Website</a>
+                                <a href="https://istudent.uitm.edu.my/">i-Student Portal</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a href="signIn.html" class="nav-link"> Sign In </a></li>
+                    <li><a href="signIn.html" class="nav-link"> Sign Out </a></li>
+                </ul>
+            </nav>
+        </div>
+    `;
+	var socialNavBar = document.createElement('div');
+	socialNavBar.innerHTML = `
+    <div class="social-nav">
+        <nav>
+            <ul>
+                <li><a href="https://www.instagram.com/" class="fa fa-instagram" target="_blank"></a></li>
+                <li>
+                    <a href="https://twitter.com/i/flow/login" class="fa fa-twitter" target="_blank"></a>
+                </li>
+                <li><a href="https://www.youtube.com/" class="fa fa-youtube" target="_blank"></a></li>
+            </ul>
+        </nav>
+    </div>
+    `;
+
+	document.body.prepend(header);
+	document.body.prepend(socialNavBar);
+}
+
+function loadFooter() {
+    var footer = document.createElement('footer');
+    var paragraph = document.createElement('p');
+
+    // Creating an anchor tag with the copyright symbol and text
+    var copyrightLink = document.createElement('a');
+    copyrightLink.innerHTML = '&copy; 2023 Farisha\'s Website. All rights reserved.';
+    copyrightLink.href = 'HP.html'; // Replace with your desired URL
+
+    // Adding a click event listener to handle the redirect
+    copyrightLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent the default link behavior
+        window.location.href = this.getAttribute('href'); // Redirect to the specified URL
+    });
+
+    // Appending the anchor tag to the paragraph, and the paragraph to the footer
+    paragraph.appendChild(copyrightLink);
+    footer.appendChild(paragraph);
+
+    // Appending the footer to the body
+    document.body.appendChild(footer);
+}
+
+// Ensure the functions are defined
+window.loadHeader = loadHeader;
+window.loadFooter = loadFooter;
